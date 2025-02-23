a742746f3decf415f50d77c065ff806daa1e60d5
sandilokaa <sandyloka790@gmail.com>
2025-02-24 01:10:10 +0700

    docs: register provider

--- config/app.php
+++ config/app.php
@@ -195,6 +195,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\SandilokaaPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\RepositoryServiceProvider::class,
 
     ],
 
