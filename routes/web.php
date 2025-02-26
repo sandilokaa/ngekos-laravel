a2092522b5da074d024b8ba0a023e90dac9561ad
sandilokaa <sandyloka790@gmail.com>
2025-02-26 14:31:33 +0700

    docs: add orders & find route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BoardingHouseController;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/check-booking', [BookingController::class, 'check'])->name('check-booking');
+Route::get('/find-boarding', [BoardingHouseController::class, 'find'])->name('find-boarding');
