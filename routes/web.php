e3b918455f4c30096b947213b4acfe3173f0b13c
sandilokaa <sandyloka790@gmail.com>
2025-02-27 23:26:52 +0700

    docs: add route

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/check-booking', [BookingController::class, 'check'])->name('check-booking');
+
 Route::get('/find-boarding', [BoardingHouseController::class, 'find'])->name('find-boarding');
+Route::get('/find-result', [BoardingHouseController::class, 'findResults'])->name('find-boarding.results');
