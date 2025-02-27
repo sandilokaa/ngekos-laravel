be2caac5594efb2b515c7fec6f78a9fdaa94b626
sandilokaa <sandyloka790@gmail.com>
2025-02-28 00:28:23 +0700

    docs: add routes category by slug

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BoardingHouseController;
 use App\Http\Controllers\BookingController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/check-booking', [BookingController::class, 'check'])->name('check-booking');
+
+Route::get('/categories/{slug}', [CategoryController::class, 'show'])->name('categories.show');
 
 Route::get('/find-boarding', [BoardingHouseController::class, 'find'])->name('find-boarding');
 Route::get('/find-result', [BoardingHouseController::class, 'findResults'])->name('find-boarding.results');
