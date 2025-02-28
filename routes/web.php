95d57e70ea47f2a0b2130f2912c645c4e86c2a49
sandilokaa <sandyloka790@gmail.com>
2025-03-01 00:38:00 +0700

    docs: add route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BoardingHouseController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CityController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,8 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/check-booking', [BookingController::class, 'check'])->name('check-booking');
 
 Route::get('/categories/{slug}', [CategoryController::class, 'show'])->name('categories.show');
+
+Route::get('/cities/{slug}', [CityController::class, 'show'])->name('city.show');
 
 Route::get('/find-boarding', [BoardingHouseController::class, 'find'])->name('find-boarding');
 Route::get('/find-result', [BoardingHouseController::class, 'findResults'])->name('find-boarding.results');
